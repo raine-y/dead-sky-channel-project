d9b4102f77f73fb92d91066b0b68e268a92db380
Tess Walker <tesshwalker@macbook-air.local>
2024-10-03 12:47:42 -0400

    worst storm since Katrina but we stay silly

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+#[tauri::command]
+fn wind_full() {
+    println!("I was invoked from JS!");
 }
 
-#[tauri::command]
-
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![wind_full])
+    .run(tauri::generate_context!())
+    .expect("failed to run app");
+}
